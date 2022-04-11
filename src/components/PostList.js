ad36b6432de9e152198d43bf7906499a1edfd54a
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-11 09:35:18 +0100

    initial PostList.js for GET request

---
+++ src/components/PostList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class PostList extends Component {
+  render() {
+    return (
+      <div>
+        <h2>List of posts</h2>
+      </div>
+    );
+  }
+}
+
+export default PostList;
