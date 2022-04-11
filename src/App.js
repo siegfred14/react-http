559f896c99e9275503d385525446427f87e3417a
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-11 09:50:48 +0100

    data fetched from api using axios library, logged in the console

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import PostList from "./components/PostList";
 
 function App() {
   return (
     <div className="App">
       <h1>initial react-http</h1>
+      <PostList />
     </div>
   );
 }
 
 export default App;
+// making a GET request using axios and rendering the fetched data in a react component
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,26 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class PostList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div>
@@ -11,3 +31,10 @@ class PostList extends Component {
 }
 
 export default PostList;
+
+// for data fetching
+// 1. import axios library
+// 2. create state property which would store the list of posts
+// 3. Use axios to make a GET request to the JSON placeholder API
+
+// since axios is a promise based library, we can add then and catch blocks
