53a0fc992de4b138066ea8936c7e2531d18c68be
Siegfred Samson <siegfredsamson@gmail.com>
2022-04-20 18:57:34 +0100

    state elements destructured and added to the value attribute

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
 
 class PostForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userId: "",
+      title: "",
+      body: "",
+    };
+  }
+
   render() {
+    const { userId, title, body } = this.state;
     return (
       <div>
         <form>
           <div>
-            <input type="text" name="userId"></input>
+            <input type="text" name="userId" value={userId}></input>
           </div>
           <div>
-            <input type="text" name="title"></input>
+            <input type="text" name="title" value={title}></input>
           </div>
           <div>
-            <input type="text" name="body"></input>
+            <input type="text" name="body" value={body}></input>
           </div>
         </form>
       </div>
